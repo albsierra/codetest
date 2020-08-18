25c6eff99ed9dd4ac0d61469830c196ba3de54aa
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-08-18 08:56:47 +0200

    Integrando TWIG en results-download

--- results-download.php
+++ results-download.php
@@ -1,40 +1,21 @@
 <?php
-
 require_once('initTsugi.php');
+
+$loader = new \Twig_Loader_Filesystem('views');
+// TODO eliminar debug 0 true y habilitar cache en tmp
+$twig = new \Twig_Environment($loader, [
+    'debug' => true
+]);
+
+$twig->addExtension(new Twig_Extensions_Extension_I18n());
 
 include('views/dao/menu.php');
 
-// Start of the output
-$OUTPUT->header();
-
-include('views/dao/tool-header.html');
-
-$OUTPUT->bodyStart();
-
-$OUTPUT->topNav($menu);
-
-echo '<div class="container-fluid">';
-
-$OUTPUT->flashMessages();
-
-$OUTPUT->pageTitle('Download Results', true, false);
-
-?>
-        <p class="lead">Click on the link below to download the student results.</p>
-        <h4>
-            <a href="actions/ExportToFile.php">
-                <span class="fa fa-download" aria-hidden="true"></span> CodeTest-<?=$CONTEXT->title?>-Results.xls
-            </a>
-        </h4>
-    </div>
-<?php
-
-$OUTPUT->helpModal("Code Test Help", __('
-                        <h4>Downloading Results</h4>
-                        <p>Click on the link to download an Excel file with all of the results for this Code Test.</p>'));
-
-$OUTPUT->footerStart();
-
-include('views/dao/tool-footer.html');
-
-$OUTPUT->footerEnd();
+echo $twig->render('answer/results-download.php', array(
+    'OUTPUT' => $OUTPUT,
+    'CONTEXT' => $CONTEXT,
+    'help' => __('
+<h4>Downloading Results</h4>
+<p>Click on the link to download an Excel file with all of the results for this Code Test.</p>'),
+    'menu' => $menu,
+));
