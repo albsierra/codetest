35b4177793d7a8cc5e6c75c9fc6f1107502d059d
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-10-12 09:41:58 +0200

    Adaptando el script de DDL para Oracle y SQLite3

--- views/question/forms/questionSQLForm.php
+++ views/question/forms/questionSQLForm.php
@@ -1,17 +1,17 @@
-                                    <label for="questionDatabase">Question Database</label>
-                                    <input type="text" name="question[question_database]" value="{{ question.getQuestionDatabase() }}">
-                                    <label for="questionDBMS">Question DBMS</label>
-                                    <select name="question[question_dbms]" id="questionDBMS">
-                                        {% for index, dbms in main.getTypeProperty('dbConnections') %}
-                                        <option value="{{ index }}" {{ question.getQuestionDbms() == index ? "selected" : "" }} . >{{ dbms.name }}</option>
-                                        {% endfor %}
-                                    </select>
                                     <label for="questionType">Question Type</label>
                                     <select name="question[question_type]" id="questionType">
                                         {% for sqlType in main.getTypeProperty('sqlTypes') %}
                                         <option value="{{ sqlType }}" {{ question.getQuestionType() == sqlType ? "selected" : "" }} . >{{ sqlType }}</option>
                                         {% endfor %}
                                     </select>
+                                    <label for="questionDBMS">Question DBMS</label>
+                                    <select name="question[question_dbms]" id="questionDBMS">
+                                        {% for index, dbms in main.getTypeProperty('dbConnections') %}
+                                        <option value="{{ index }}" {{ question.getQuestionDbms() == index ? "selected" : "" }} . >{{ dbms.name }}</option>
+                                        {% endfor %}
+                                    </select>
+                                    <label for="questionDatabase">Question Database</label>
+                                    <input type="text" name="question[question_database]" value="{{ question.getQuestionDatabase() }}">
                                     <label for="questionSolution">Question Solution</label>
                                     <textarea class="form-control" name="question[question_solution]" rows="4" required>{{ question.getQuestionSolution() }}</textarea>
                                     <label for="questionProbe">Question Probe</label>
