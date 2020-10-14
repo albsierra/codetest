be57d81cfdaabab9f4288037345cdcd620b22126
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-10-14 10:46:44 +0200

    Hace que los datos de entrada se muestren en <pre>

--- views/question/students/questionCodeStudent.php
+++ views/question/students/questionCodeStudent.php
@@ -2,7 +2,7 @@
                                     <span><strong>Language:&nbsp;</strong></span><span>{{ main.getTypeProperty('codeLanguages')[question.getQuestionLanguage()].name }}</span>
                                 </p>
                                 <p class="h4 inline flx-cntnr flx-row flx-nowrap flx-start">
-                                    <span><strong>Input: </strong></span><p>{{ question.getQuestionInputTest() }}</p>
+                                    <span><strong>Input: </strong></span><pre>{{ question.getQuestionInputTest() }}</pre>
                                 </p>
                                 <p class="h4 inline flx-cntnr flx-row flx-nowrap flx-start">
                                     <span><strong>Output: </strong></span><pre>{{ question.getQuestionOutputTest() }}</pre>
