94961762f9c9e35a74d197d3851a3bd49448bf68
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2021-12-03 09:03:06 +0100

    Improve exercises list

--- exercises-list.php
+++ exercises-list.php
@@ -1,4 +1,8 @@
 <?php
+
+use CT\CT_ExerciseCode;
+use CT\CT_ExerciseSQL;
+
 require_once('initTsugi.php');
 
 include('views/dao/menu.php');
@@ -6,11 +10,24 @@ include('views/dao/menu.php');
 $main = new \CT\CT_Main($_SESSION["ct_id"]);
 
 $exercises = $main->getExercises();
+$typedExercises = array();
 
+if($main->getType() == '1'){
+    foreach($exercises as $el){
+        array_push($typedExercises, new CT_ExerciseCode($el->getExerciseId()));
+    }
+}else{
+    foreach($exercises as $el){
+        array_push($typedExercises, new CT_ExerciseSQL($el->getExerciseId()));
+    }
+}
+
+// var_dump($exercises);die;
 // var_dump($gradesMap);die;
 
 echo $twig->render('pages/exercises-list.php.twig', array(
-    'exercises' => $exercises,
+    'mainType' => $main->getType(),
+    'exercises' => $typedExercises,
     'OUTPUT' => $OUTPUT,
     'CFG' => $CFG,
     'menu' => $menu,
