45ab5b914b899890aa3eec83791d615e7bd9a3ac
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-02-01 16:42:48 +0100

    Add tests to student view from centralRepository

--- student-home.php
+++ student-home.php
@@ -12,7 +12,14 @@ $toolTitle = $main->getTitle() ? $main->getTitle() : "Code Test";
 $exercises = $main->getExercises();
 $totalExercises = count($exercises);
 
+$firstExerciseId = $exercises[0]->getAkId();
+
 $currentExerciseNumber = isset($_GET['exerciseNum']) ? $_GET['exerciseNum'] : 1;
+
+$exerciseTestsResponse = $REST_CLIENT_REPO->getClient()->request('GET', "api/exercises/$firstExerciseId/tests");
+$testsResponseObj = $exerciseTestsResponse->toArray();
+
+$testsObj = sizeof($testsResponseObj) > 0 ? $testsResponseObj[0] : null;
 
 $user = new \CT\CT_User($USER->id);
 
@@ -22,6 +29,7 @@ echo $twig->render('pages/student-view.php.twig', array(
     'menu' => $menu,
     'user' => $user,
     'exercises' => $exercises,
+    'testsObj' => $testsObj,
     'totalExercises' => $totalExercises,
     'currentExerciseNumber' => $currentExerciseNumber,
     'exerciseNum' => $currentExerciseNumber,
