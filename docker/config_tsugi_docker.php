acbb61d5e6f70f4e5a258f8f3949bc5d89d2eb0f
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-02-07 13:33:49 +0100

    Move data to env variables

--- docker/config_tsugi_docker.php
+++ docker/config_tsugi_docker.php
@@ -34,9 +34,9 @@ $apphome = false;
 if ( $apphome ) {
     $wwwroot = $apphome . '/tsugi';
 } else if ( U::get($_SERVER,'SERVER_PORT') == 8888 ) {
-    $wwwroot = 'http://localhost:8888/tsugi'; // Mac XAMP
+    $wwwroot = 'http://'.(getenv('TSUGI_DOMAIN') ?: 'localhost').':8888/tsugi'; // Mac XAMP
 } else {
-    $wwwroot = "http://localhost/tsugi";
+    $wwwroot = 'http://'.(getenv('TSUGI_DOMAIN') ?: 'localhost').'/tsugi';
 }
 // Once you are on a real server delete the above if statement
 // and set the wwwroot directly.  This must be the actual URL used
